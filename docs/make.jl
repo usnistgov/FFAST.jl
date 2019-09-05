a8acd1c9a2e759247e1cc71683d0df389f4db915
Nicholas Ritchie <NicholasWMRitchie@users.noreply.github.com>
2019-09-05 19:51:06 -0400

    Added framework based on Example.jl

---
+++ docs/make.jl
@@ -0,0 +1,5 @@
+using Documenter, FFAST
+
+makedocs(modules = [FFAST], sitename = "FFAST.jl")
+
+deploydocs(repo = "github.com/NicholasWMRitchie/FFAST.jl.git")
---
+++ docs/src/index.md
@@ -0,0 +1,7 @@
+# FFAST
+
+* An implementation of Chantler's FFAST MACs
+
+```@autodocs
+Modules = [FFAST]
+```
